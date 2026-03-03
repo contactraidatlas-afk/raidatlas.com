3e5cc4dc03d1541c44599d55c818182df66e51bf
EL MEHDI BOUABBADI <ebouabbadi@gmail.com>
2026-03-03 16:02:31 +0000

    commit change

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,12 @@ const nextConfig = {
     formats: ["image/avif", "image/webp"],
     deviceSizes: [320, 420, 640, 768, 1024, 1280, 1536, 1920, 2048, 2560],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
   },
 }
 
